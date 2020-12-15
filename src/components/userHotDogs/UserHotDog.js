e903ecbd71b13daa720b0124f0cc2f6b36c9eae5
Shawn Conerton <conerton52@gmail.com>
2020-12-15 10:44:48 -0600

    set up module wall of weiners. Not working code yet.

--- src/components/userHotDogs/UserHotDog.js
+++ src/components/userHotDogs/UserHotDog.js
@@ -7,26 +7,24 @@ export const UserHotDog = ({ userHotDog, props }) => {
   const { userHotDogs, getUserHotDogById, deleteUserHotDog } = useContext(
     UserHotDogContext
   );
-  // const [userHotDog, setUserHotDog] = useState({ hotdog: {} });
-
-  // useEffect(() => {
-  //   console.log("USER HOT DOG", userHotDog);
-  //   console.log("SET USER HOTDOGS", setUserHotDog);
-  //   console.log("GET USER HOT BY ID", getUserHotDogById);
-  //   getUserHotDogById(
-  //     props.match.params.userHotDogId
-  //   ).then((parsedUserHotDogs) => setUserHotDog(parsedUserHotDogs));
-  //   console.log("PROPS DELETE", props);
-  // }, []);
-
-  //this part of my code is broken and I have no idea why. It is not liking the Link. Cannot read 'id'
+  const [checked, setChecked] = React.useState(false);
   return (
     <section className="userHotDog">
       <div className="user_hotDog_name">
         <Link to={`/userHotDogs/${userHotDog.id}`}>{userHotDog.hotDogId}</Link>
       </div>
       <form>
-        <div className="is_favorite"> Favorite?: </div>
+        <label>
+          Favorite?:{" "}
+          <input
+            type="radio"
+            name="favHotDog"
+            value="favHotDog"
+            checked={checked}
+            className="is_favorite"
+            onChange={() => setChecked(!checked)}
+          />{" "}
+        </label>
       </form>
       <button
         className="delete_button"
@@ -39,3 +37,17 @@ export const UserHotDog = ({ userHotDog, props }) => {
     </section>
   );
 };
+
+// const [userHotDog, setUserHotDog] = useState({ hotdog: {} });
+
+// useEffect(() => {
+//   console.log("USER HOT DOG", userHotDog);
+//   console.log("SET USER HOTDOGS", setUserHotDog);
+//   console.log("GET USER HOT BY ID", getUserHotDogById);
+//   getUserHotDogById(
+//     props.match.params.userHotDogId
+//   ).then((parsedUserHotDogs) => setUserHotDog(parsedUserHotDogs));
+//   console.log("PROPS DELETE", props);
+// }, []);
+
+//this part of my code is broken and I have no idea why. It is not liking the Link. Cannot read 'id'
---
+++ src/components/wallOfWeiners/WallOfWeiners.js
@@ -0,0 +1,14 @@
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import "./WallOfWeiners.css";
+
+export const User = () => {
+  <section className="wallOfWeiners_UserCard">
+    <div>
+      Name: {user.name}
+      Time it took to Complete:{}
+      Favorite Hot Dog: {user.favHotDog}
+      Date Complete:{}
+    </div>
+  </section>;
+};
