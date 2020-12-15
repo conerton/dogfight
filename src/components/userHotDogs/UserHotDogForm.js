dee50eac7539ebed0d0fa1602114333b3e1f9cfd
Shawn Conerton <conerton52@gmail.com>
2020-12-14 21:59:06 -0600

    Unix time is now converted to a readable time.

--- src/components/userHotDogs/UserHotDogForm.js
+++ src/components/userHotDogs/UserHotDogForm.js
@@ -6,21 +6,10 @@ import "./UserHotDog.css";
 export const UserHotDogForm = (props) => {
   const { getUserHotDogsById, userHotDog } = useContext(UserHotDogContext);
 
-  //   const { getHotDogById } = useContext(HotDogContext);
-  //   const [hotDog, setHotDog] = useState({});
-
   useEffect(() => {
     getUserHotDogsById(+props.match.params.userHotDogId);
   }, []);
 
-  //   useEffect(() => {
-  //     console.log("PROPS", props);
-  //     // debugger;
-  //     getHotDogById(props.match.params.hotDogId).then((parsedHotDog) => {
-  //       setHotDog(parsedHotDog);
-  //       console.log("parsedHotDog", parsedHotDog);
-  //     });
-  //   }, []);
   console.log("PROPS", props, userHotDog);
   return (
     <>
@@ -29,7 +18,10 @@ export const UserHotDogForm = (props) => {
         {" "}
         Toppings: {userHotDog.hotDog.topping}
       </div>
-      <div className="time_stamp"> Date: {userHotDog.timeStamp}</div>
+      <div className="time_stamp">
+        {" "}
+        Date: {new Date(userHotDog.timeStamp).toLocaleDateString("en-us")}
+      </div>
       {/* <div className="is_favorite"> Favorite?: {userHotDog.favHotDog}</div> */}
       <div className="note"> Notes: {userHotDog.hotDogNote}</div>
       {/* <button>Delete</button> */}
