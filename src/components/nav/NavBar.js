e8581df61ad686a2505dc53daa93cd883f333109
Shawn Conerton <conerton52@gmail.com>
2021-01-04 15:57:12 -0600

    final styling 01/04/2020

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 import logo from "../img/CorisLogo.png";
 
+const clearLocalStorage = () => {
+  localStorage.removeItem("dogfight_user_id");
+};
+
 export const NavBar = (props) => {
   return (
     <div className="navbar">
@@ -23,6 +27,17 @@ export const NavBar = (props) => {
         <div>
           <Link className="navbar_wall" to="/wallOfWeiners">
             Wall Of Weiners
+          </Link>
+        </div>
+        <div>
+          <Link
+            className="logout"
+            onClick={() => {
+              clearLocalStorage();
+              props.history.push("/login");
+            }}
+          >
+            Log Out
           </Link>
         </div>
       </div>
